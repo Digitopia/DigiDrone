b60b685bc25033b50e7ec956614dbc1ffd8401d3
Nuno Hespanhol <nuno.hespanhol@gmail.com>
2019-07-30 19:04:07 +0100

    Fix 100vh in mobile because of address bar
    More info:
    https://css-tricks.com/the-trick-to-viewport-units-on-mobile/

--- src/App.vue
+++ src/App.vue
@@ -491,6 +491,9 @@ export default {
     },
 
     resize() {
+      const vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
+
       this.previousWidth = this.width
       this.previousHeight = this.height
       this.width = this.$el.clientWidth
@@ -626,7 +629,7 @@ html {
 .wrapper {
   position: absolute;
   width: 100%;
-  height: 100vh;
+  height: calc(var(--vh, 1vh) * 100);
 }
 #canvas {
   position: absolute;
