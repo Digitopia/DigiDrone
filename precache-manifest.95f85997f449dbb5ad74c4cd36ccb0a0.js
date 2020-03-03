6fb20a233e04ce051d353dd88fc8fffaa4e446a8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-03 17:37:41 +0000

    Deploy Digitopia/DigiDrone to github.com/Digitopia/DigiDrone.git:gh-pages

---
+++ precache-manifest.95f85997f449dbb5ad74c4cd36ccb0a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af2da8753a09d8b73358",
+    "url": "/DigiDrone/css/app.661f396e.css"
+  },
+  {
+    "revision": "3011c756cc213f10d603490e60c0609d",
+    "url": "/DigiDrone/index.html"
+  },
+  {
+    "revision": "af2da8753a09d8b73358",
+    "url": "/DigiDrone/js/app.d401fbb3.js"
+  },
+  {
+    "revision": "47c02e329d27814d3191",
+    "url": "/DigiDrone/js/chunk-vendors.fe16c480.js"
+  },
+  {
+    "revision": "3c41bd2578d66cc312f970779d50a57e",
+    "url": "/DigiDrone/manifest.json"
+  },
+  {
+    "revision": "f71d20196d4caf35b6a670db8c70b03d",
+    "url": "/DigiDrone/robots.txt"
+  }
+]);
